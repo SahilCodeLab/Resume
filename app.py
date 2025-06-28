e6b8427eb2c0e9ec254a4e888903597041ac1322
CodeWithSahil <fair29565@gmail.com>
2025-06-28 21:56:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from flask import Flask, render_template, request, send_file, url_for
-import pdfkit
-import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
-# Configure pdfkit
-PDFKIT_CONFIG = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -25,18 +20,11 @@ def index():
         template_choice = request.form['template']
         template_file = f"template{template_choice}.html"
 
-        rendered = render_template(template_file, data=data)
-
-        with open("resume_temp.html", "w", encoding='utf-8') as f:
-            f.write(rendered)
-
-        pdfkit.from_file("resume_temp.html", "resume.pdf", configuration=PDFKIT_CONFIG)
-
-        return send_file("resume.pdf", as_attachment=True)
+        return render_template(template_file, data=data)
 
     return render_template("form.html")
 
-
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
